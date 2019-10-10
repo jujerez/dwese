6d9cc5181cb36fd40dc1b1bd0dc8c70b7bb6b4d6
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2019-10-10 21:18:52 +0200

    fff

---
+++ saludo.php
@@ -0,0 +1,20 @@
+<?php   
+
+echo 25 ;
+echo 25.4;
+echo "Hola";
+// expresiones
+echo 25 / 3; // devuelve un real
+echo 4 / 2; // devuelve un entero
+// funciones
+echo cos(2);
+echo max(4, 9);
+echo cos(max(2,4,1,7,5,9));
+print(25*3); // pinta el resultado  y ademas devuelve 1
+
+$hola = "Hola";
+
+$hola = 25;
+
+// varibles case sensitive
+// funciones no case sensitive
