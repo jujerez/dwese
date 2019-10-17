50f863ef71f7a7c8c1c2a615fda6accc8bedd0bd
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2019-10-17 19:59:19 +0200

    uso de require_once en one.php

--- uno.php
+++ uno.php
@@ -1,6 +1,6 @@
 <?php
 echo "Estoy en el aarchivo uno.php \n";
 
-require 'dos.php';
-require 'tres.php';                // Se llamara 2 veces al tres ya que el dos tiene una llamada al tres
+require_once 'dos.php';
+require_once 'tres.php';                // Se llamara 2 veces al tres ya que el dos tiene una llamada al tres
 echo "Sigo en el uno.php";
