3fd61b62cd26be975fd79443347aec57fd6e4d10
Juan Antonio Jerez <juanantonio.jerez@iesdonana.org>
2019-10-17 20:00:38 +0200

    Cambio el comentario en uno.php

--- uno.php
+++ uno.php
@@ -2,5 +2,4 @@
 echo "Estoy en el aarchivo uno.php \n";
 
 require_once 'dos.php';
-require_once 'tres.php';                // Se llamara 2 veces al tres ya que el dos tiene una llamada al tres
-echo "Sigo en el uno.php";
+require_once 'tres.php';                // ahora solo lo carga una sola vez
